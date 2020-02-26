178529e89497aeb9ec36f70f6185d92a7c76642b
vitezslav-ruzicka <vitekvin@seznam.cz>
2020-02-26 21:43:36 +0100

    BUG FIXED: the variable binString was not rewrited but new string was appended to the end of the string which caused problems with memory and printing values from this string.
    Signed-off-by: vitezslav-ruzicka <vitekvin@seznam.cz>

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ public:
 
     void read_buffer (int receiverBuffer[])
     {
+        binString.clear();
         bin_to_string(receiverBuffer);
     }
 
@@ -37,7 +38,7 @@ public:
 private:
     string binString;
     int *binArr;
-
+    //converts 8bit int to binary array
     int* int8_to_bin (int number)
     {
         static int buffer[8];
@@ -54,6 +55,8 @@ private:
         return buffer;
     }
 
+    //converts whole raw sbus receiver channel to
+    //one long binary number stored in string
     string bin_to_string(int receiverBuffer[])
     {
         for (int i = 1; i < 13; i++)
@@ -62,13 +65,15 @@ private:
             binArr = int8_to_bin(receiverBuffer[i]);
             for (int x = 0; x < 8; x++)
             {
-                //append the binary to one big string
+                //append the binary to one big string dru
                 binString += to_string(*(binArr + x));
             }
         }
         return binString;
     }
 
+    //cuts the long binary string from previous function
+    //and converts it to integer
     int get_channel(int pos)
     {
         //goes to exact location in the binary array and convert
@@ -78,19 +83,26 @@ private:
         //convert the binary string to an integer
         return stoi(aura, 0, 2);
     }
+
+    int get_flags(char receiverBuffer[])
+    {
+        
+    }
 };
 
 int main() {
     //test receiverBuffer
-    int arr[25] = {12, 15, 43, 434,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,90};
+    //arr[0] = start bit(240d, F0h, 11110000b)
+    int arr[25] = {240, 15, 43, 434,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,90};
     receiver sbus;
 
-    //read the buffer and convert them into channels
-    sbus.read_buffer(arr);
-    //get the 2nd channel
-    int chan = sbus.channel(8);
-    //print the channel
-    printf("%d ",chan);
-
+    while (1) {
+        //read the buffer and convert them into channels
+        sbus.read_buffer(arr);
+        //get the 2nd channel
+        int chan = sbus.channel(2);
+        //print the channel
+        printf("%d \n", chan);
+    }
     return 0;
 }
