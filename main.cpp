0199e809fce948d0b7b434a245c9954f0278d444
vitezslav-ruzicka <vitekvin@seznam.cz>
2020-03-06 22:31:29 +0100

    New class SBUS added - testing better performance, stability improved
    Signed-off-by: vitezslav-ruzicka <vitekvin@seznam.cz>

--- main.cpp
+++ main.cpp
@@ -2,11 +2,68 @@
 #include <string.h>
 using namespace std;
 
+class SBUS {
+
+public:
+    int receiverBuffer[25];
+
+    void read_buffer (char buffer[])
+    {
+        //converts and saves the buffer to internal one
+        for (int i =0; i < 25; i++)
+        {
+            receiverBuffer[i] = (int)buffer[i];
+        }
+    }
+
+    //should be repeated 12 times 1-13
+    void int8_to_string (int number)
+    {
+        unsigned char help = 128;
+
+        for (int i = 0; i < 8; i++)
+        {
+            if (receiverBuffer[number] & help)
+            {
+                binOut += to_string(1);
+            } else {
+                binOut += to_string(0);
+            }
+            help >>=1;
+        }
+    }
+
+    //enter
+    int channel (int channel)
+    {
+        //only 8 channels are scanned
+        if (channel > 8 || channel < 1)
+        {
+            return -1;
+        }
+
+        return get_channel(channel);
+    }
+
+private:
+    string binOut;
+
+    int get_channel (int pos)
+    {
+        //goes to exact location in the binary array and convert
+        //next 12bits to int
+        // (1 channel is 12bit long but its transmitted by 8bits)
+        string aura = binOut.substr(pos * 12, 12);
+        //convert the binary string to an integer
+        return stoi(aura, 0, 2);
+    }
+};
 
 class receiver {
 
 public:
     bool flags[2];
+    string binString;
 
     /*
      * TO DO:
@@ -17,13 +74,16 @@ public:
 
     void read_buffer (int receiverBuffer[])
     {
+        //clears the string -> before cycles were appended to the end of the string
+        //bluepill run out of memory
         binString.clear();
         bin_to_string(receiverBuffer);
     }
 
+    //get the channel
     int channel(int number)
     {
-        //number -1 because arrays starts with :)
+        //number -1 because arrays starts with 0 :)
         number--;
 
         //only 8 channels are scanned
@@ -36,8 +96,8 @@ public:
     }
 
 private:
-    string binString;
     int *binArr;
+
     //converts 8bit int to binary array
     int* int8_to_bin (int number)
     {
@@ -93,15 +153,21 @@ private:
 int main() {
     //test receiverBuffer
     //arr[0] = start bit(240d, F0h, 11110000b)
-    int arr[25] = {240, 15, 43, 434,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,90};
-    receiver sbus;
+    char arr[25] = {20, 15, 43, 34,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,90};
+    int chan;
+    //testing the class
+    SBUS sbus;
 
-    while (1) {
-        //read the buffer and convert them into channels
+    while (1)
+    {
         sbus.read_buffer(arr);
-        //get the 2nd channel
-        int chan = sbus.channel(2);
-        //print the channel
+
+        for (int i = 1; i < 13; i++)
+        {
+            sbus.int8_to_string(i);
+        }
+
+        chan = sbus.channel(1);
         printf("%d \n", chan);
     }
     return 0;
